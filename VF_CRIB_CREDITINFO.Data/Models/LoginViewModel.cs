a372cf6534170a91bba20b8d9d9a5246e72a2442
4537 <4537@VF-CO-IT-DEV-11>
2026-03-25 13:58:31 +0530

    AD auth business logic altered

---
+++ VF_CRIB_CREDITINFO.Data/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VF_CRIB_CREDITINFO.Data.Models
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
