5c36fea88816783d6fae847f897517b3f245c255
4537 <4537@VF-CO-IT-DEV-11>
2026-03-26 16:42:40 +0530

    CRIB Search Ui altered to code

---
+++ VF_CRIB_CREDITINFO.Presentation/Controllers/CRIBController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VF_CRIB_CREDITINFO.Presentation.Controllers
+{
+    public class CRIBController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
