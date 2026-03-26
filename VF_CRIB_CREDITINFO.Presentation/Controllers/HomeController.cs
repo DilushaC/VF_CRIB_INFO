737cc85ec6ec1bdc6eac645c25b16e15b4725de1
4514 <4514@VF-CO-IT-DEV-01>
2026-03-26 08:55:41 +0530

    Search CRIB

--- VF_CRIB_CREDITINFO.Presentation/Controllers/HomeController.cs
+++ VF_CRIB_CREDITINFO.Presentation/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace VF_CRIB_CREDITINFO.Presentation.Controllers
             return View();
         }
 
+        public ActionResult SearchCRIB()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
