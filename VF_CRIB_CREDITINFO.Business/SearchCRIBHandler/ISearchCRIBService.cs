e1893f74bbfdac78900810f12a9f81ac1cdc499b
4514 <4514@VF-CO-IT-DEV-01>
2026-03-27 14:09:30 +0530

    New Search Development

---
+++ VF_CRIB_CREDITINFO.Business/SearchCRIBHandler/ISearchCRIBService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VF_CRIB_CREDITINFO.Data.Models;
+
+namespace VF_CRIB_CREDITINFO.Business.SearchCRIBHandler
+{
+    public interface ISearchCRIBService
+    {                
+        Task<string> SearchCRIBData(string numberType, string number, bool isIndividual, string token);
+    }
+}
---
+++ VF_CRIB_CREDITINFO.Presentation/Controllers/CRIBController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VF_CRIB_CREDITINFO.Business.SearchCRIBHandler;
+
+namespace VF_CRIB_CREDITINFO.Presentation.Controllers
+{
+    public class CRIBController : Controller
+    {
+        private readonly ISearchCRIBService _SearchCRIBService;
+
+        public CRIBController(ISearchCRIBService SearchCRIBService)
+        {
+            _SearchCRIBService = SearchCRIBService;
+        }
+
+        public IActionResult SearchCRIB()
+        {
+            return View();
+        }       
+      
+
+        [HttpPost]
+        public async Task<JsonResult> SearchData(string numberType, string number, bool isIndividual)
+        {
+            string token = "TOKEN_Number"; 
+
+            var result = await _SearchCRIBService.SearchCRIBData(
+                numberType,
+                number,
+                isIndividual,
+                token
+            );
+
+            return Json(result);
+        }
+
+    }
+}
