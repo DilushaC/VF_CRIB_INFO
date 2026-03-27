bb094e973d5b77561aca25288ebf657f890136ff
4537 <4537@VF-CO-IT-DEV-11>
2026-03-27 17:09:23 +0530

    CRIB data search implemented at initial stage

--- VF_CRIB_CREDITINFO.Presentation/Controllers/CRIBController.cs
+++ VF_CRIB_CREDITINFO.Presentation/Controllers/CRIBController.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VF_CRIB_CREDITINFO.Business.CRIBHandler;
+using VF_CRIB_CREDITINFO.Business.SearchCRIBHandler;
 
 namespace VF_CRIB_CREDITINFO.Presentation.Controllers
 {
     public class CRIBController : Controller
     {
         private readonly ITokenService _tokenService;
+        private readonly  ICRIBService _CRIBService;
 
-        public CRIBController(ITokenService tokenService)
+        public CRIBController(ITokenService tokenService,ICRIBService CRIBService)
         {
             _tokenService = tokenService;
+            _CRIBService = CRIBService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             const string serviceURL = "https://identity.cbsnext.domain/connect/token/";
@@ -27,6 +31,22 @@ namespace VF_CRIB_CREDITINFO.Presentation.Controllers
             ViewBag.Token = tokenData;
 
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<JsonResult> SearchData(string numberType, string number, bool isIndividual)
+        {
+            string token = "TOKEN_Number";
+
+            var result = await _CRIBService.SearchCRIBData(
+                numberType,
+                number,
+                isIndividual,
+                token
+            );
+
+            return Json(result);
         }
     }
 }
